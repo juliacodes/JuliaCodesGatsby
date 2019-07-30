e057d75e234e94c0b5b4d0cc51c7b907d5b9257d
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-07-30 16:16:09 +0200

    Initial Push

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/juliajohnson/JuliaCodesGatsby/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/src/pages/page-2.js")))
+}
+
