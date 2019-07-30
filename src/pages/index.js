510122bf540c919e57c0222c1d795b7f9998fac6
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-07-30 16:25:33 +0200

    Added styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "gatsby";
-
+import styled from "styled-components";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
+const Hello = styled.div`
+  color: blue;
+`;
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi me</h1>
+    <Hello>Hi me</Hello>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
