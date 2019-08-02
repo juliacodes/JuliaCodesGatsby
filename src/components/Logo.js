385e30e786d3fd0d4d8d7cace3fae69a287045ae
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-08-02 21:04:12 +0200

    Messing with hover events

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
 import logosvg from "../images/Logo.svg";
-
 import styled from "styled-components/macro";
 
+const LogoImg = styled.div`
+  position: relative;
+  background-image: url(${logosvg});
+  width: 34px;
+  height: 32px;
+  background-repeat: no-repeat;
+
+  /* ::after {
+    opacity: 0;
+    z-index: -2;
+    content: "";
+    display: inline-block;
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    top: -10px;
+    left: -10px;
+    border-radius: 30px;
+    background-color: black;
+    transition-duration: 0.2s;
+  }
+
+  :hover ::after {
+    opacity: 1;
+  }
+
+  svg > g > g > .LogoMain{
+    fill: white;
+  }
+
+  :hover{
+      color: white;
+  } */
+`;
 function Logo() {
-  return <img src={logosvg} alt="Logo" />;
+  return <LogoImg />;
 }
 
 export default Logo;
