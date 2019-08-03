f168e7bd572c6a0fe73c962d53e92c259d1736eb
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-08-03 12:50:53 +0200

    Deletd more unused components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
 import { breakpoints } from "../../Media";
-import SEO from "../components/seo";
 
 const Layout = styled.div`
   display: flex;
@@ -18,7 +17,6 @@ const Layout = styled.div`
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
