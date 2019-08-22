18c9a7a788ef672493ee327ffeeff927eb3226af
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-08-22 10:17:53 -0400

    Added Course Content

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,7 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/src/pages/404.js"))),
   "component---src-pages-awkward-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/src/pages/Awkward.js"))),
+  "component---src-pages-cgt-270-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/src/pages/CGT270.js"))),
   "component---src-pages-forecast-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/src/pages/Forecast.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/juliajohnson/JuliaCodesGatsby/src/pages/index.js")))
 }
