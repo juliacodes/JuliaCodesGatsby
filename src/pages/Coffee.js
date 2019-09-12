eaa0ce153a5b849fad4bb070537cfca4e97dd1ae
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-09-12 00:56:17 -0400

    Copy Fixes

--- src/pages/Coffee.js
+++ src/pages/Coffee.js
@@ -258,12 +258,12 @@ function Forecast(props) {
           <TextContent>
             <Heading>Coffee By Benjamin</Heading>
             <Paragraph>
-              Forecast is a project I put together after learning the basics of
-              GraphQL. This application was the perfect way to improve my
-              understanding of making queries and managing larger sets of data.
-              In this project, I utilized OpenWeatherMap API to gather the
-              weather data and injected this data based on the user's zip code
-              input.
+              Coffee by Benjamin is a React Application built for a self
+              roasting coffee kit. I built this project from scratch alongside a
+              designer with React, GraphQL, and Shopify. This e-commerce
+              application required a lot of heavy lifting to create a universal
+              cart and overall shopping experience as well as introduce the
+              product and include support pages.
             </Paragraph>
             <ProjectTable>
               <tbody>
@@ -313,13 +313,17 @@ function Forecast(props) {
           <TextContent>
             <Heading>Project Purpose and Goal</Heading>
             <Paragraph>
-              As the main purpose of this project was to improve my
-              comprehension of GraphQL and APIs in general, I spend a good
-              amount of time making sure I was using the best practices of state
-              management and data manipulation. Besides this, I also wanted to
-              make this a weather application that I would enjoy using, meaning
-              it had to be visually pleasing and allow the user to see their
-              weather status at a quick glance.
+              This project included 3 phases and iterations of the site. Phase 1
+              simply allowed users to enter their email to be alerted to when
+              the product was released. Phase 2 was quite larger and is designed
+              to introduce users to the Coffee By Benjamin product and answer
+              any questions they may have. Phase three is by far the largest and
+              most complex, as it includes the full shop and cart pages as well
+              as the logic and backend that goes along with it. <br />
+              <br />I found that the best way to implement these 3 phases
+              without having separate versions saved was to incorporate a
+              feature flag that will pass the current state that should be
+              displayed and then render content conditionally.
             </Paragraph>
           </TextContent>
           <WebStack>
@@ -327,18 +331,18 @@ function Forecast(props) {
             <RightContent>
               <Heading>Web Stack and Explanation</Heading>
               <Paragraph>
-                For this project I used React, but many different Javascript
-                libraries could've been used, and even vanilla Javascript could
-                have been easily implemented. I chose to use React because of
-                its ease of use and the ability to smoothly manipulate states
-                without having to re-render the pages.
+                React made the most sense for the web application because it
+                required to connect to graphQL and the Shopify-SDK for
+                javascript ties into React very smoothly. The Shopify-Buy-SDK
+                was chosen because of the ability for the client to modify the
+                products without any complex coding knowledge.
               </Paragraph>
               <Paragraph>
-                GraphQL was mainly chosen because I wanted to improve my
-                abilities with it, but also because their server performance is
-                extremely fast, and I knew there wouldn't be any wait time. To
-                publish the application, I hosted it through Netlify, because
-                its easy, free and how I've hosted many of my projects.
+                React hooks and session storage are also used throughout the
+                project to maintain the user cart items and allows the cart
+                count and other shopping data to be displayed universally
+                without the need for Redux. Netlify is also an obvious choice
+                for deployment because of its speed and reliability.
               </Paragraph>
             </RightContent>
           </WebStack>
@@ -349,16 +353,14 @@ function Forecast(props) {
           <CenterText>
             <Heading>Problems and Thought Process</Heading>
             <Paragraph>
-              I normally work through coding projects and logic by creating
-              lists like above. Here I've written down some necessary elements
-              that my program needs to find a solution for. I also note a few
-              complications that arose once my app was up and running. <br />{" "}
-              <br /> In this case, I noticed that it would grow tedious for a
-              user to have to enter and re-enter their zip code over and over,
-              so I saved their first entry to Localhost. Another small problem
-              was making the proper icon render, as the data I was pulling had
-              no quick numerical reference for the icons, I was forced to have
-              the icons render based on the object that described the weather.
+              Like most projects, I ran into a few bumps along the way, one
+              particularly difficult area was organization and structure of the
+              code. Because of this project's size, I realized how important
+              maintaining an organized structure would be.
+              <br /> <br /> I worked hard to keep components as reusable as
+              possible and utilized props for many slight variations. I also
+              used styled-components, because the structure of CSS-in-js is much
+              clearer and prevents overrides.
             </Paragraph>
           </CenterText>
           <Image
@@ -366,15 +368,17 @@ function Forecast(props) {
             alt="Forecast image with differing weather information"
           />
           <TextContent>
-            <Heading>Lessons Learned from Project</Heading>
+            <Heading>Lessons Learned</Heading>
             <Paragraph>
-              I still have a few features I would love to implement with this
-              project. I'd really like to update the background images so that
-              they display based on what the weather status currently is, so
-              that the application has the feel of the weather they're currently
-              experiencing. I also have plans to create secondary zip-codes, so
-              the user can click back and forth between two locations and
-              compare the weather.
+              I could spend all day describing the lessons that I learned while
+              working on this project, but the most important ones involved my
+              newfound understanding of React Hooks, Git management, Feature
+              Keys, and API integration. As my first large project using React,
+              I learned a lot of lessons regarding code structure and
+              organization. When I first began, I would write sloppy code and
+              move on, but now I know how doing so can come back to bite you; I
+              now spend a lot more time refactoring and improving every line I
+              code I write, for the best readability and far fewer headaches.
             </Paragraph>
           </TextContent>
         </Container>
