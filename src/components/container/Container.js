05368cd274ca973e4b85231084f6a56aa5721ebc
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2020-01-11 17:45:38 -0500

    Mobile Style Changes

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,7 +1,8 @@
-import styled from "styled-components/macro";
+import styled from 'styled-components/macro';
 
 export const Container = styled.div`
-  width: 100%;
-  margin: 100px auto;
+    width: 100%;
+    margin: 100px auto;
+    flex-wrap: wrap;
 `;
 export const FullWidthContainer = styled.div``;
