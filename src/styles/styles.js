2148ad9544934963a1d11d1d9cccd62dbef6063e
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2020-02-04 14:52:56 -0500

    Separated Heading Wrap into its own component

---
+++ src/styles/styles.js
@@ -0,0 +1,27 @@
+import styled, { keyframes } from 'styled-components';
+
+export const QUERIES = {
+    medium: `min-width: 1092px`,
+    small: `min-width: 552px`,
+    maxWidth: `1500px`
+};
+
+export const Paragraph = styled.p`
+    margin: 50px 0;
+    color: #696969;
+    width: 100%;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 184.69%;
+    letter-spacing: 0.03em;
+
+    a {
+        border-bottom: 2px solid transparent;
+        transition: 0.3s;
+
+        :hover {
+            border-bottom: 2px solid black;
+        }
+    }
+`;
