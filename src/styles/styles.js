2a0ba7380984c64b909dd9dd324e02ce7bb30994
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2020-02-04 19:51:11 -0500

    Separated HeadingWrap

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,5 +1,5 @@
 import styled, { keyframes } from 'styled-components';
-
+import { breakpoints } from '../components/Media';
 export const QUERIES = {
     medium: `min-width: 1092px`,
     small: `min-width: 552px`,
@@ -23,5 +23,32 @@ export const Paragraph = styled.p`
         :hover {
             border-bottom: 2px solid black;
         }
+    }
+`;
+
+export const HeadingMain = styled.h1`
+    font-weight: 900;
+    font-size: 7vw;
+    line-height: 129.69%;
+    letter-spacing: 0.03em;
+    margin-bottom: 0;
+    margin-left: 0px;
+
+    @media (min-width: ${breakpoints.mobileMax}) {
+        font-size: 60px;
+        margin-bottom: 0;
+        margin-left: 50px;
+    }
+
+    > span {
+        color: #b4e0e8;
+    }
+`;
+
+export const HeadingSec = styled(HeadingMain)`
+    font-size: 33px;
+
+    @media (min-width: ${breakpoints.mobileMax}) {
+        font-size: 40px;
     }
 `;
