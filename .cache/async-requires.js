e7b4d76b6fb3730dbdeba676a6efacafb203e438
juliajcodes <john2122@purdue.edu>
2020-06-14 13:51:56 -0400

    added babel plugin

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
