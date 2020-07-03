f6d083ae0f6901ab651a7ce5daacaed79b56e618
juliajcodes <john2122@purdue.edu>
2020-07-03 14:07:11 -0400

    Added media queries to footer

--- src/components/foot/styles.js
+++ src/components/foot/styles.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-import { Full, Heading, Paragraph } from '../../theming/styles';
+import QUERIES, { Full, Heading, Paragraph } from '../../theming/styles';
 
 export const FootCont = styled.footer`
     width: 100%;
-    height: 800px;
     background-color: ${({ theme }) => theme.alt};
 
     * {
@@ -11,28 +10,105 @@ export const FootCont = styled.footer`
     }
 
     ${Full} {
+        margin-top: 0;
+        margin-bottom: 0;
         padding-top: 200px;
+        padding: 100px 30px 0 30px;
+
+        @media (${QUERIES.large}) {
+            padding-top: 200px;
+        }
     }
 
     ${Heading} {
-        font-size: 50px;
+        font-size: 25px;
+        @media (${QUERIES.large}) {
+            font-size: 50px;
+        }
     }
 
     ${Paragraph} {
         color: ${({ theme }) => theme.textLight};
-        font-size: 20px;
+        font-size: 15px;
         max-width: 700px;
+        @media (${QUERIES.large}) {
+            font-size: 20px;
+        }
     }
 
     a {
-        font-size: 20px;
+        color: ${({ theme }) => theme.textMain};
+        font-size: 15px;
         font-weight: 600;
         padding-bottom: 10px;
-        border-bottom: 3px solid black;
+        border-bottom: 3px solid ${({ theme }) => theme.textMain};
         transition: all 0.3s ease-in-out;
         :hover {
-            background-position: 0px 0px;
-            border-bottom: 0px solid #c2e99c;
+            border-bottom: 3px solid transparent;
         }
+
+        @media (${QUERIES.large}) {
+            font-size: 20px;
+        }
+    }
+`;
+
+export const FootFinal = styled.div`
+    display: flex;
+    width: 100%;
+    height: 130px;
+    padding: 0;
+    margin: 150px 0 0 0;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    @media (${QUERIES.large}) {
+        flex-wrap: nowrap;
+        margin: 250px 0 0 0;
+        height: 100px;
+    }
+
+    ${Paragraph} {
+        margin-left: 30px;
+        font-size: 15px;
+        font-weight: 500;
+        color: ${({ theme }) => theme.textMain};
+
+        @media (${QUERIES.large}) {
+            margin-left: 50px;
+        }
+    }
+`;
+
+export const FinalLeft = styled.div`
+    display: flex;
+    width: 100%;
+    height: 50px;
+    padding: 0;
+    align-items: center;
+`;
+
+export const FinalRight = styled(FinalLeft)`
+    justify-content: flex-start;
+    ${Paragraph} {
+        margin-left: 0;
+        margin-right: 20px;
+    }
+
+    @media (${QUERIES.large}) {
+        justify-content: flex-end;
+    }
+`;
+
+export const LogoSvg = styled.svg`
+    transform: scale(0.7);
+
+    * {
+        fill: ${({ theme }) => theme.textMain};
+        stroke: ${({ theme }) => theme.textMain};
+    }
+
+    @media (${QUERIES.large}) {
+        transform: scale(1);
     }
 `;
