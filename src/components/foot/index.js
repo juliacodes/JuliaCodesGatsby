a90504cc6499b3cc0d31dabe4caf11846bebea7b
juliajcodes <john2122@purdue.edu>
2020-07-03 14:07:11 -0400

    Added media queries to footer

--- src/components/foot/index.js
+++ src/components/foot/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { FootCont } from './styles';
-import Logo from '../logo';
+import { FootCont, FootFinal, LogoSvg, FinalLeft, FinalRight } from './styles';
 import { Full, Heading, Paragraph } from '../../theming/styles';
 import Button from '../button';
 const Foot = () => {
@@ -13,6 +12,34 @@ const Foot = () => {
                     Duis ac pretium magna. elit. Duis ac pretium magna.{' '}
                 </Paragraph>
                 <a href=''>julia@juliacodes.com</a>
+                <FootFinal>
+                    <FinalLeft>
+                        <LogoSvg
+                            width='34'
+                            height='56'
+                            viewBox='0 0 34 56'
+                            fill='none'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path
+                                d='M17.7771 37.4649V1H33.3557V21.1186C33.3557 31.1778 24.9672 36.2075 17.7771 37.4649Z'
+                                stroke-width='1.25741'
+                            />
+                            <path
+                                d='M1 38.6538H16.5786V55.0001C6.95946 52.4595 3.34397 49.1068 1 38.6538Z'
+                                stroke-width='1.25741'
+                            />
+                        </LogoSvg>
+                        <Paragraph>
+                            Designed and Built by Julia Johnson
+                        </Paragraph>
+                    </FinalLeft>
+                    <FinalRight>
+                        <Paragraph>github</Paragraph>
+                        <Paragraph>twitter</Paragraph>
+                        <Paragraph>instagram</Paragraph>
+                    </FinalRight>
+                </FootFinal>
             </Full>
         </FootCont>
     );
