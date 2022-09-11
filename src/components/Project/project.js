4528f675b3722bffb8bfc5d9fa2db1a0da69de82
juliajcodes <john2122@purdue.edu>
2022-09-11 14:43:54 -0500

    routing working

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -1,15 +1,16 @@
 import QUERIES, { Heading, Paragraph } from '../../theming/styles';
 import { CoffeeChemistry } from '../../images';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Project = ({
     name = 'Project Name',
     description = 'Passionate about facilitating excellent user experiences through site speed optimization, accessibility, and user testing. ',
-    url,
+    url = 'project',
     image = CoffeeChemistry,
 }) => {
     const ImageProj = styled.div`
-        margin: 100px 0;
+        margin: 50px 0;
         background-image: url(${image});
         width: 100%;
         height: 400px;
@@ -30,17 +31,19 @@ const Project = ({
     `;
     return (
         <Project>
-            <Heading
-                style={{
-                    textAlign: 'left',
-                    marginLeft: 0,
-                    marginRight: 0,
-                    width: '100%',
-                    maxWidth: 1000,
-                }}
-            >
-                {name}
-            </Heading>
+            <Link to={`/${url}`}>
+                <Heading
+                    style={{
+                        textAlign: 'left',
+                        marginLeft: 0,
+                        marginRight: 0,
+                        width: '100%',
+                        maxWidth: 1000,
+                    }}
+                >
+                    {name}
+                </Heading>
+            </Link>
             <Paragraph style={{ maxWidth: 650, marginTop: 20 }}>
                 {description}
             </Paragraph>
