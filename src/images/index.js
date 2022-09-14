0805f65b9ee19a8d9bd24b27d77ae6f8622b21a0
juliajcodes <john2122@purdue.edu>
2022-09-14 12:32:01 -0500

    adding all content

--- src/images/index.js
+++ src/images/index.js
@@ -2,5 +2,46 @@ import Awk1 from '../images/Awkward1.jpg';
 import Movie from '../images/movie.png';
 import Vuln from '../images/vuln.png';
 import Devops from '../images/devops.png';
+import {
+    Change1,
+    Change2,
+    Change3,
+    Lofi,
+    Hifi,
+    JourneyMap,
+    Person1,
+    Person2,
+    Research1,
+    Research2,
+    Research3,
+    Sketch1,
+    Sketch2,
+    Sketch3,
+    Sketch4,
+    UserFlow,
+    Synth,
+} from './movie-theater';
 
-export { Awk1, Movie, Vuln, Devops };
+export {
+    Awk1,
+    Movie,
+    Vuln,
+    Devops,
+    Change1,
+    Change2,
+    Change3,
+    Lofi,
+    Hifi,
+    JourneyMap,
+    Person1,
+    Person2,
+    Research1,
+    Research2,
+    Research3,
+    Sketch1,
+    Sketch2,
+    Sketch3,
+    Sketch4,
+    UserFlow,
+    Synth,
+};
